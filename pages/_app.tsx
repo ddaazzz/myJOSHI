f114d9a86cdb3ba1bdb8a17200f51bc0fe98c8e3
ddaazzz <darrentang5316@gmail.com>
2025-10-15 03:06:16 +0800

    Integrate Dynamic.xyz wallet connection and upgrade React for Web3 support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import { config, library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-config.autoAddCss = false
-library.add(fab)
+
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from '@dynamic-labs/sdk-react-core';
+import { SolanaWalletConnectors } from '@dynamic-labs/solana';
+
+config.autoAddCss = false;
+library.add(fab);
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId: 'b6555669-5bd5-43e3-a960-664f88e4082f',
+        walletConnectors: [SolanaWalletConnectors],
+      }}
+    >
+      <DynamicWidget />
+      <Component {...pageProps} />
+    </DynamicContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
